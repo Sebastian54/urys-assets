82c8de0c91b428ced5d91e463d7335bead2aa0d3
Sebastian <45011245+Sebastian54@users.noreply.github.com>
2024-10-02 12:41:00 +0100

    Initial commit

---
+++ src/main/java/com/seba542/urysassets/UrysAssets.java
@@ -0,0 +1,16 @@
+package com.seba542.urysassets;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UrysAssets implements ModInitializer {
+	public static final String MOD_ID = "urys-assets";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/seba542/urysassets/UrysAssetsClient.java
@@ -0,0 +1,10 @@
+package com.seba542.urysassets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UrysAssetsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
